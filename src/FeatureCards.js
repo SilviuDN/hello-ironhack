02db764a622a75878df6b44a6767d5c98337dba3
SilviuDN <silviudilimot@gmail.com>
2023-01-28 13:55:25 +0100

    Iterate over featureCards

--- src/FeatureCards.js
+++ src/FeatureCards.js
@@ -1,32 +1,22 @@
 import FeatureCard from "./FeatureCard";
+import { Component } from "react";
+import { featureCards } from './fakeAPI';
 
-import icon1 from './icon1.png'
-import icon2 from './icon2.png'
-import icon3 from './icon3.png'
-import icon4 from './icon4.png'
+class FeatureCards extends Component{
+    constructor(){
+        super()
+        this.state = {
+            features: featureCards
+        }
+    }
 
-const FeatureCards = (props) => {
-
-    // const innerJSX = ''
-
-    // props.featureCards.forEach(el => {
-    //     innerJSX += <FeatureCard title = {el.title} description = {el.description} imageSource = {el.imageSource}/>
-    // });
-
-    return(
-        <>
-            <FeatureCard title = "Declarative" description = "React makes it painless to create interactive UIs." imageSource = {icon1}/>
-            <FeatureCard title = "Components" description = "Build encapsuled components that manage their state." imageSource = {icon2}/>
-            <FeatureCard title = "Single-Way" description = "A set of imutable values is passed to the components." imageSource = {icon3}/>
-            <FeatureCard title = "JSX" description = "Statically typed, designed to run on modern browsers." imageSource = {icon4}/>
-
-            {/* {
-                props.featureCards.forEach( el => <FeatureCard title = "{el.title}" description = "{el.description}" imageSource = {icon1}/>)
-            } */}
-
-        </>
-
-    )
+    render(){
+        // const {title, description, imageSource} = this.state
+        return(
+            this.state.features.map( (el,idx) => <FeatureCard key = {idx} title={el.title} description={el.description} imageSource={el.imageSource}/>)
+        )
+    }
 }
+
 
 export default FeatureCards
---
+++ src/fakeAPI.js
@@ -0,0 +1,12 @@
+import icon1 from './icon1.png'
+import icon2 from './icon2.png'
+import icon3 from './icon3.png'
+import icon4 from './icon4.png'
+
+
+export const featureCards = [
+    { title: "Declarative", description: "React makes it painless to create interactive UIs.", imageSource: {icon1}},
+    { title: "Components", description: "Build encapsuled components that manage their state.", imageSource: {icon2}},
+    { title: "Single-Way", description: "A set of imutable values is passed to the components.", imageSource :{icon3}},
+    { title: "JSX", description: "Statically typed, designed to run on modern browsers.", imageSource: {icon4}}
+  ]
